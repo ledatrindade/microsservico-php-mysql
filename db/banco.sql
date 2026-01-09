7999ec9d20c1cc95bc876cf577271940a4752ab5
Lêda Trindade <ledatrindade94@gmail.com>
2026-01-09 08:20:17 -0300

    Adicionando todos os arquivos do projeto Docker

---
+++ db/banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dados (
+    AlunoID int AUTO_INCREMENT PRIMARY KEY,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
---
+++ php/index.php
@@ -0,0 +1,34 @@
+<?php
+ini_set("display_errors", 1);
+header('Content-Type: text/html; charset=iso-8859-1');
+
+echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+
+// Conexão com o MySQL no container
+$servername = "db";
+$username = "root";
+$password = "Senha123";
+$database = "meubanco";
+
+// Criar conexão
+$link = new mysqli($servername, $username, $password, $database);
+
+if ($link->connect_error) {
+    die("Falha na conexão: " . $link->connect_error);
+}
+
+$valor_rand1 = rand(1, 999);
+$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$host_name = gethostname();
+
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) 
+          VALUES ('$valor_rand1', '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2', '$host_name')";
+
+if ($link->query($query) === TRUE) {
+    echo "Novo registro criado com sucesso!";
+} else {
+    echo "Erro: " . $link->error;
+}
+
+$link->close();
+?>
